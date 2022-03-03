b73bf8cd41472b632607cfb7cffc8d62f8109b49
ZarVee <zarvee7@gmail.com>
2022-03-03 19:50:49 +0500

    course

--- src/main/java/com/example/zarchat/HelloController.java
+++ src/main/java/com/example/zarchat/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.zarchat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private TextArea tipArea;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField AnswerField;
+
+
+    public void sending(ActionEvent actionEvent) {
+        final String answer = AnswerField.getText();
+        if(answer != null && !answer.isEmpty()){
+            tipArea.requestFocus();
+            tipArea.appendText(answer +"\n");
+        }
+        AnswerField.clear();
     }
+
 }
